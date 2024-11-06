d6f332a6d62155c1fcfa67155607d42f8caad358
ramyaraghavendra80 <118874458+ramyaraghavendra80@users.noreply.github.com>
2024-11-06 16:33:49 +0530

    final commit4

--- onestopbank/onestopbank/settings.py
+++ onestopbank/onestopbank/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-fl=2kos_(m-#va#3vqo$*z@%bvstaomu614y3i6u283wm!em$%
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [*]
+ALLOWED_HOSTS = ['*']
 
 LOGIN_URL = '/login/'
 # Application definition
